276ac8186ea8070b7633fafb6987a9e2123a7615
tnguy753 <nguyenthao989@gmail.com>
2022-08-15 15:54:45 +0700

    Initial Commit

---
+++ Assignment2-ThaoN/Assignment2_ThaoNApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment2_ThaoNApp.swift
+//  Assignment2-ThaoN
+//
+//  Created by Nguyễn Thảo on 15/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Assignment2_ThaoNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
